0a713c0d1b6f7d8bf8c7f5b724774522ebdecee6
Allan Feid <afeid@shutterstock.com>
2012-07-05 18:23:06 -0400

    simple kernel extension to bring require_relative functionality to 1.8.x

---
+++ lib/core_ext/kernel.rb
@@ -0,0 +1,8 @@
+# This is a hack that brings Ruby 1.9.x functionality into 1.8.x
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
