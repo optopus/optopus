ae0bcb3444b0664a192a477c8aa9bcc3b3ed306a
Allan <allanfeid@gmail.com>
2012-07-06 01:35:25 -0400

    fix up validations

--- models/appliance.rb
+++ models/appliance.rb
@@ -1,13 +1,15 @@
 class Appliance < ActiveRecord::Base
   include AttributesToLiquidMethodsMapper
 
-  set_primary_key :uuid
-  validates :serial_number, :primary_mac_address, :presence => true
-  before_create :assign_uuid
+  validates :serial_number, :primary_mac_address, :uuid, :presence => true
+  validates_uniqueness_of :uuid
+  before_validation :assign_uuid
 
   private
 
   def assign_uuid
-    self.uuid = UUIDTools::UUID.md5_create(UUIDTools::UUID_DNS_NAMESPACE, "#{self.serial_number.downcase} #{self.primary_mac_address.downcase}").to_s
+    unless self.serial_number.nil? or self.primary_mac_address.nil?
+      self.uuid = UUIDTools::UUID.md5_create(UUIDTools::UUID_DNS_NAMESPACE, "#{self.serial_number.downcase} #{self.primary_mac_address.downcase}").to_s
+    end
   end
 end
