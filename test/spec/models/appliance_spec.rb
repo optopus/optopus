2a0b1ae8e30af932e6e434862aa0ca69452895cc
Allan <allanfeid@gmail.com>
2012-07-06 00:36:42 -0400

    use variables for valid mac and serial

--- test/spec/models/appliance_spec.rb
+++ test/spec/models/appliance_spec.rb
@@ -1,17 +1,23 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 describe Appliance, '#new' do
+  before(:all) do
+    @valid_mac_address = '01:23:45:67:89:ab'
+    @valid_ip_address = '10.10.10.10'
+    @valid_serial_number = 'testserial'
+  end
+
   it 'fails to save without primary_mac_address' do
-    appliance = Appliance.new(:serial_number => 'testserial')
+    appliance = Appliance.new(:serial_number => @valid_serial_number)
     expect { appliance.save! }.to raise_error(ActiveRecord::RecordInvalid)
   end
 
   it 'fails to save without serial_number' do
-    appliance = Appliance.new(:primary_mac_address => '01:23:45:67:89:ab')
+    appliance = Appliance.new(:primary_mac_address => @valid_mac_address)
     expect { appliance.save! }.to raise_error(ActiveRecord::RecordInvalid)
   end
 
   it 'saves successfully when supplied a serial_number and primary_mac_address' do
-    appliance = Appliance.new(:serial_number => 'testserial', :primary_mac_address => '01:23:45:67:89:ab')
+    appliance = Appliance.new(:serial_number => @valid_serial_number, :primary_mac_address => @valid_mac_address)
     appliance.save!
   end
 end
