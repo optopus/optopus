a95e42aec49e9cec779ae3f05f76a9efbdfd3a06
Allan <allanfeid@gmail.com>
2012-07-08 11:04:11 -0400

    ensure virtual is set to true or false and assign an appliance before saving a physical node

--- models/node.rb
+++ models/node.rb
@@ -3,8 +3,10 @@ module Optopus
     include AttributesToLiquidMethodsMapper
 
     validates :uuid, :hostname, :serial_number, :primary_mac_address, :presence => true
+    validates :virtual, :inclusion => { :in => [true, false] }
     validates_uniqueness_of :uuid
     before_validation :assign_uuid
+    before_save :assign_appliance
     belongs_to :appliance
 
     private
@@ -12,6 +14,14 @@ module Optopus
     def assign_uuid
       unless self.serial_number.nil? or self.primary_mac_address.nil?
         self.uuid = "#{self.serial_number.downcase} #{self.primary_mac_address.downcase}".to_md5_uuid
+      end
+    end
+
+    def assign_appliance
+      if self.virtual
+        # TODO: determine best way to associate an appliance with virtual nodes
+      else
+        self.appliance = Appliance.where(:uuid => self.uuid).first
       end
     end
   end
