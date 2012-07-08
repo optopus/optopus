d2b353aeb275f871f09874e4265303ff0703bb1d
Allan <allanfeid@gmail.com>
2012-07-08 11:04:40 -0400

    add a basic test for confirming node to appliance association

---
+++ test/spec/models/node_and_appliance_association_spec.rb
@@ -0,0 +1,21 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+describe 'Optopus::Node and Optopus::Appliance associations' do
+  before(:all) do
+    @valid_mac_address = '01:23:45:67:89:ad'
+    @valid_ip_address = '10.10.10.10'
+    @valid_serial_number = 'testserial3'
+  end
+
+  it 'physical node becomes associated with appliance on creation when matching uuid is found' do
+    appliance = Optopus::Appliance.new(:serial_number => @valid_serial_number, :primary_mac_address => @valid_mac_address)
+    appliance.save!
+    node = Optopus::Node.new(
+      :hostname => 'test.host',
+      :serial_number => @valid_serial_number,
+      :primary_mac_address => @valid_mac_address,
+      :virtual => false
+    )
+    node.save!
+    node.appliance_id.should == appliance.id
+  end
+end
