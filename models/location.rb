44d22a5bc12a346e928aee57e43ec74084933e4a
Allan Feid <afeid@shutterstock.com>
2012-07-26 14:44:28 -0400

    search for nodes based on a fact called location

--- models/location.rb
+++ models/location.rb
@@ -5,5 +5,9 @@ module Optopus
     validates_uniqueness_of :common_name
 
     has_many :appliances
+
+    def nodes
+      Optopus::Node.where("facts -> 'location' = '#{self.common_name}'")
+    end
   end
 end
