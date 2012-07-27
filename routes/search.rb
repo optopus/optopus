8c855b8f972ccfc825fe3aac9ee963c2d97dccf7
Allan Feid <afeid@shutterstock.com>
2012-07-27 06:00:07 -0400

    default to AND searching when multiple strings specified

--- routes/search.rb
+++ routes/search.rb
@@ -3,20 +3,28 @@ module Optopus
     get '/search' do
       validate_param_presence 'query'
       # since we suck at using elasticsearch, split on the first : and assume someone searched for column_name:something
-      query_parts = params['query'].gsub(/[\{\}\[\]\(\)]/) { |s| "\\#{s}" }.split(':', 2)
-      field = query_parts.first
-      query = query_parts.last
-      if query.nil?
-        query_string = field
-      else
-        query = "\"#{query}\"" unless query.include?('*')
-        query_string = "#{field}:#{query}"
+      query_string = ''
+      params['query'].split.each do |and_query|
+        query_parts = and_query.gsub(/[\{\}\[\]\(\)]/) { |s| "\\#{s}" }.split(':', 2)
+        field = query_parts.first
+        query = query_parts.last
+        if query.nil?
+          query_string += field + ' '
+        else
+          query = "\"#{query}\"" unless query.include?('*')
+          query_string += "#{field}:#{query}" + ' '
+        end
       end
-      node_results = Optopus::Node.search(query_string, :size => 2000).sort { |a,b| a.hostname <=> b.hostname }
-      appliance_results = Optopus::Appliance.search(query_string, :size => 2000)
+      query_string.chomp!
+      node_results = Optopus::Node.search(:size => 2000) do
+        query { string query_string, :default_operator => 'AND' }
+      end
+      appliance_results = Optopus::Appliance.search(:size => 2000) do
+        query { string query_string, :default_operator => 'AND' }
+      end
       @search_query = params['query']
       @results = Array.new
-      @results << { :type => :node, :results => node_results }
+      @results << { :type => :node, :results => node_results.sort { |a,b| a.hostname <=> b.hostname } }
       @results << { :type => :appliance, :results => appliance_results }
       erb :search_results
     end
