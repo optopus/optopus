18bc9c2179eabfc768c0e764ceb3b494bb59c5ac
Allan Feid <afeid@shutterstock.com>
2012-07-27 03:06:26 -0400

    try to get basic search functionality going

--- routes/search.rb
+++ routes/search.rb
@@ -2,10 +2,24 @@ module Optopus
   class App
     get '/search' do
       validate_param_presence 'query'
+      #query = params['query'].gsub(/[\{\}\[\]\(\)]/) { |s| "\\#{s}" } + '*' # quick escaping
+      # since we suck at using elasticsearch, split on the first : and assume someone searched for column_name:something
+      query_parts = params['query'].gsub(/[\{\}\[\]\(\)]/) { |s| "\\#{s}" }.split(':', 2)
+      field = query_parts.first
+      query = query_parts.last
+      query = "\"#{query}\"" unless query.include?('*')
+
       @search_query = params['query']
       @results = Array.new
-      @results << { :type => :node, :results => Optopus::Node.search(params['query']) }
-      @results << { :type => :appliance, :results => Optopus::Appliance.search(params['query']) }
+      node_results = Optopus::Node.search(:size => 2000) do 
+        query do
+          string "#{field}:#{query}"
+        end
+
+        #(query, :size => 200)
+      end
+      @results << { :type => :node, :results => node_results.sort { |a,b| a.hostname <=> b.hostname } }
+      @results << { :type => :appliance, :results => Optopus::Appliance.search("#{field}:#{query}") }
       erb :search_results
     end
   end
