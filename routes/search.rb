e1d719e55e495fa51e9c6f87b75dbbfb808cae84
Ryan Peterson <ryan.m.peterson@gmail.com>
2012-07-26 23:11:22 -0400

    added search my main damie

--- routes/search.rb
+++ routes/search.rb
@@ -1,8 +1,13 @@
 module Optopus
   class App
     get '/search' do
-      subnav_from_locations
-      erb :search
+      validate_param_presence 'query'
+      @results = Array.new
+      puts params['query']
+      @results << Optopus::Node.search(params['query'])
+      @results << Optopus::Appliance.search(params['query'])
+      p @results
+      erb :search_results
     end
   end
 end
