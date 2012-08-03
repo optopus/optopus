fe61d6a37a2f24ec39a8d743e1ca25f8c1ef0302
Allan Feid <allanfeid@gmail.com>
2012-08-03 14:45:04 -0400

    remove useless plugin

--- plugins/test/init.rb
+++
@@ -1,8 +0,0 @@
-module Optopus
-  module Plugin
-    module Test
-      extend Optopus::Plugin
-      require_relative 'model'
-    end
-  end
-end
--- plugins/test/model.rb
+++
@@ -1,8 +0,0 @@
-module Optopus
-  module Plugin
-    module Test
-      class TestModel < Optopus::Plugin::Model
-      end
-    end
-  end
-end
