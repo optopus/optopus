03f5db1a881a3a5cf78ec82609210cb690f86191
Allan <allanfeid@gmail.com>
2012-08-02 23:28:14 -0400

    create has and belongs to many association between users and roles

--- models/role.rb
+++ models/role.rb
@@ -3,5 +3,6 @@ module Optopus
     validates :name, :presence => true
     validates_uniqueness_of :name
     serialize :properties, ActiveRecord::Coders::Hstore
+    has_and_belongs_to_many :users
   end
 end
--- models/user.rb
+++ models/user.rb
@@ -3,5 +3,6 @@ module Optopus
     validates :username, :display_name, :presence => true
     validates_uniqueness_of :username
     serialize :properties, ActiveRecord::Coders::Hstore
+    has_and_belongs_to_many :roles
   end
 end
