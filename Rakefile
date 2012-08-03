43973a8862b4a7a88ca885631f1eb15d5aff6c48
Allan <allanfeid@gmail.com>
2012-08-02 23:04:13 -0400

    test user model

--- Rakefile
+++ Rakefile
@@ -52,6 +52,7 @@ task :test do
   Optopus::Node.destroy_all
   Optopus::Device.destroy_all
   Optopus::Location.destroy_all
+  Optopus::User.destroy_all
   Rake::Task['db:migrate'].invoke
   Rake::Task['spec'].invoke
 end
--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,7 @@
 module Optopus
   class User < ActiveRecord::Base
     validates :username, :display_name, :presence => true
+    validates_uniqueness_of :username
     serialize :properties, ActiveRecord::Coders::Hstore
   end
 end
