0a41967fdbf266da8bba041c28e31b8cd8622b79
Allan <allanfeid@gmail.com>
2012-08-04 19:55:54 -0400

    create views into user management

--- models/user.rb
+++ models/user.rb
@@ -4,5 +4,9 @@ module Optopus
     validates_uniqueness_of :username
     serialize :properties, ActiveRecord::Coders::Hstore
     has_and_belongs_to_many :roles
+
+    def member_of?(role_id)
+      roles.where(:id => role_id).first != nil
+    end
   end
 end
---
+++ routes/admin.rb
@@ -0,0 +1,51 @@
+module Optopus
+  class App
+    before '/admin/*', :auth => :admin do; end
+
+    get '/admin/users' do
+      @modify_users = Optopus::User.all
+      erb :users
+    end
+
+    get '/admin/user/:id/roles' do
+      @roles = Optopus::Role.all
+      @modify_user = Optopus::User.where(:id => params[:id]).first
+      if @modify_user.nil?
+        flash[:error] = 'User does not exist.'
+        redirect back
+      end
+      erb :modify_user_roles, :layout => false
+    end
+
+    post '/admin/user/:id/roles' do
+      @modify_user = Optopus::User.where(:id => params[:id]).first
+      if @modify_user.nil?
+        flash[:error] = 'User does not exist.'
+        redirect back
+      end
+
+      new_roles = Array.new
+      params.each do |key, value|
+        if key =~ /^role_(.*)/
+          role = Optopus::Role.where(:id => value).first
+          next if role.nil?
+          new_roles << role
+        end
+      end
+      @modify_user.roles = new_roles
+      redirect back
+    end
+
+    delete '/admin/user/:id' do
+      user = Optopus::User.where(:id => params[:id]).first
+      begin
+        raise 'User does not exist.' if user.nil?
+        user.destroy
+        flash[:success] = "Deleted #{user.username} successfully!"
+        redirect back
+      rescue Exception => e
+        handle_error(e)
+      end
+    end
+  end
+end
