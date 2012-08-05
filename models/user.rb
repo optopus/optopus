d5a424de8b0deff781c4ce9280acba13630594dd
Allan <allanfeid@gmail.com>
2012-08-05 14:33:28 -0400

    make it easy to find all events associated with a user

--- models/user.rb
+++ models/user.rb
@@ -6,6 +6,10 @@ module Optopus
     serialize :properties, ActiveRecord::Coders::Hstore
     has_and_belongs_to_many :roles
 
+    def events
+      Optopus::Event.where("properties -> 'user_id' = '#{id}'").order('created_at DESC')
+    end
+
     def member_of?(role_id)
       roles.where(:id => role_id).first != nil
     end
