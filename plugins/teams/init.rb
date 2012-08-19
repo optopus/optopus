932cd52ccc1aa18864891d7a5943eb9a5a68ccc1
Allan <allanfeid@gmail.com>
2012-08-19 18:24:20 -0400

    register team admin menu

--- plugins/teams/init.rb
+++ plugins/teams/init.rb
@@ -4,9 +4,14 @@ module Optopus
     module Teams
       extend Optopus::Plugin
 
+      teams_admin_menu = Optopus::Menu::Section.new(:name => 'teams_admin_menu')
+      teams_admin_menu.add_link :display => 'Teams', :href => '/teams/admin'
+      teams_admin_menu.required_role = 'teams_admin'
+
       plugin do
         register_mixin :roles, TeamsMethods::Role
         register_role 'teams_admin'
+        register_menu teams_admin_menu
       end
 
       get '/teams/admin', :auth => 'teams_admin' do
