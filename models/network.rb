fdbb73f5c7cfffc9ff47e0280abfc95ddc4d7eba
Allan Feid <allanfeid@gmail.com>
2012-08-27 15:48:39 -0400

    associate lone addresses with a network on save

--- models/network.rb
+++ models/network.rb
@@ -2,5 +2,17 @@ module Optopus
   class Network < Optopus::Model
     validates :address, :presence => true
     has_many :addresses
+
+    before_save :assign_addresses
+
+    private
+
+    # Associate addresses that are contained in this network, but
+    # only if they do not have a network assigned already
+    def assign_addresses
+      Optopus::Address.where(:network_id => nil).where('ip_address << ?', self.address.to_cidr).each do |address|
+        self.addresses << address
+      end
+    end
   end
 end
--- test/spec/models/network_spec.rb
+++ test/spec/models/network_spec.rb
@@ -16,6 +16,12 @@ describe Optopus::Network, '#new' do
   it 'fails to save when duplicate network is defined' do
     expect { Optopus::Network.create!(:address => @valid_network) }.to raise_error(ActiveRecord::RecordNotUnique)
   end
+
+  it 'associates lone addresses with network on save' do
+    address = Optopus::Address.create!(:ip_address => '10.10.10.3')
+    network = Optopus::Network.create!(:address => '10.10.10.0/24')
+    network.addresses.first.id.should == address.id
+  end
 end
 
 describe Optopus::Address, '#new' do
