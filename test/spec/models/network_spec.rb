394dbdcfeba4349c0837423c73f0bee870d8a575
Allan Feid <allanfeid@gmail.com>
2012-08-27 15:07:00 -0400

    add basic network model tests

---
+++ test/spec/models/network_spec.rb
@@ -0,0 +1,49 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+describe Optopus::Network, '#new' do
+  before(:all) do
+    @valid_network = '10.0.1.0/23'
+  end
+
+  it 'fails to save without network address' do
+    expect { Optopus::Network.create! }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'saves successfully when supplied a network address' do
+    Optopus::Network.create!(:address => @valid_network)
+  end
+
+  it 'fails to save when duplicate network is defined' do
+    expect { Optopus::Network.create!(:address => @valid_network) }.to raise_error(ActiveRecord::RecordNotUnique)
+  end
+end
+
+describe Optopus::Address, '#new' do
+  before(:all) do
+    @valid_address = '10.1.1.2'
+    @address = Optopus::Address.new(:ip_address => @valid_address)
+    @network = Optopus::Network.create!(:address => '10.1.1.0/23')
+  end
+
+  it 'fails to save without ip_address' do
+    expect { Optopus::Address.create! }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'saves successfully when supplied ip_address' do
+    @address.save!
+  end
+
+  it 'associates with a valid network on save' do
+    @address.network.id.should == @network.id
+  end
+
+  it 'fails to save when duplicate ip_address is defined' do
+    expect { Optopus::Address.create!(:ip_address => @valid_address) }.to raise_error(ActiveRecord::RecordNotUnique)
+  end
+
+  it 'fails to save if assigned network does not contain the assigned ip_address' do
+    @address.reload
+    @address.network = Optopus::Network.create!(:address => '192.168.1.0/24')
+    expect { @address.save! }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+end
