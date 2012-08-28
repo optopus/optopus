b4aa6f1bbc895f3edf9a643fac88cc142dcd3a70
Allan <allanfeid@gmail.com>
2012-08-27 22:30:08 -0400

    basic view into networks by locationn with the ability to create new networks

--- routes/network.rb
+++ routes/network.rb
@@ -1,8 +1,37 @@
 module Optopus
   class App
     get '/networks' do
-      @networks = Optopus::Network.all
+      subnav_from_locations
       erb :networks
+    end
+
+    put '/network' do
+      begin
+        validate_param_presence 'network-address', 'network-bits', 'network-location-id'
+        unless params['network-bits'].to_i.between?(1, 32)
+          raise 'network bits must be an integer between 1 and 32'
+        end
+
+        unless params['network-address'].match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/)
+          raise 'invalid network address specified, must contain four octets'
+        end
+
+        location = Optopus::Location.find_by_id(params['network-location-id'])
+        raise 'invalid location' if location.nil?
+        network = Optopus::Network.new(
+          :address     => "#{params['network-address']}/#{params['network-bits']}",
+          :vlan_id     => params['network-vlan-id'],
+          :description => params['network-description']
+        )
+        network.location = location
+        network.save!
+      rescue Exception => e
+        handle_error(e)
+      end
+
+      status 201
+      flash[:success] = "Successfully created new network for #{network.address.to_cidr}!"
+      redirect back
     end
 
     get '/network/:id' do
@@ -11,7 +40,7 @@ module Optopus
         status 404
         flash[:error] = "Network with id #{params[:id]} does not exist!"
       end
-      # TODO: create view into this data
+      erb :network
     end
   end
 end
