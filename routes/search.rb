3a7bac08d0e5b9301d777d4c7f45f889eb8b300f
Allan Feid <allanfeid@gmail.com>
2012-09-17 13:57:32 -0400

    make it easy to show the timestamps of items in a search result

--- routes/search.rb
+++ routes/search.rb
@@ -4,6 +4,9 @@ module Optopus
       begin
         validate_param_presence 'query'
         @search_query = params['query']
+        if params.include?('with-timestamps') && params['with-timestamps'] == 'true'
+          @timestamps = true
+        end
         @results = Optopus::Search.query(params['query'])
         erb :search_results
       rescue Exception => e
