3a1e2372a7fdff6a6bc5ba68f26ef59d5b0156e7
Allan Feid <allanfeid@gmail.com>
2012-09-25 13:04:37 -0400

    crud for pods

--- routes/admin.rb
+++ routes/admin.rb
@@ -2,9 +2,87 @@ module Optopus
   class App
     admin_menu = Optopus::Menu::Section.new(:name => 'admin_menu', :required_role => 'admin')
     admin_menu.add_link :display => 'Users', :href => '/admin/users'
+    admin_menu.add_link :display => 'Pods', :href => '/admin/pods'
     Optopus::Menu.register_section(admin_menu)
 
+    def pod_from_params
+      @pod = Optopus::Pod.find_by_id(params[:id])
+      if @pod.nil?
+        status 404
+        flash[:error] = "Pod with id #{params[:id]} does not exist!"
+      end
+    end
+
     before '/admin/*', :auth => :admin do; end
+
+    get '/admin/pods' do
+      subnav_from_locations
+      erb :pods
+    end
+
+    post '/admin/pod/:id/edit' do
+      begin
+        pod = Optopus::Pod.find_by_id(params[:id])
+        raise 'Pod does not exist!' if pod.nil?
+
+        if params['pod-name']
+          register_event "{{ references.user.to_link }} renamed pod #{pod.name} to #{params['pod-name']} in #{pod.location.common_name}", :type => 'pod', :references => [ pod ]
+          pod.name = params['pod-name']
+          pod.save!
+        end
+      rescue Exception => e
+        handle_error(e)
+      end
+
+      flash[:success] = "Successfully updated #{pod.name} in #{pod.location.common_name}!"
+      redirect back
+    end
+
+    put '/admin/pod' do
+      begin
+        validate_param_presence 'pod-name', 'pod-location-id'
+        location = Optopus::Location.find_by_id(params['pod-location-id'])
+        raise 'invalid location' if location.nil?
+        pod = location.pods.create!(:name => params['pod-name'])
+        register_event "{{ references.user.to_link }} created pod #{pod.name} in #{pod.location.common_name}", :type => 'pod', :references => [ pod ]
+      rescue Exception => e
+        handle_error(e)
+      end
+
+      status 201
+      flash[:success] = "Successfully created new pod in #{pod.location.common_name} called #{pod.name}!"
+      redirect back
+    end
+
+    delete '/admin/pod/:id' do
+      begin
+        pod_from_params
+        raise 'Pod does not exist!' if @pod.nil?
+        @pod.destroy
+        register_event "{{ references.user.to_link }} deleted pod #{@pod.name} in #{pod.location.common_name}", :type => 'pod'
+      rescue Exception => e
+        handle_error(e)
+      end
+      flash[:success] = "Successfully deleted #{@pod.name} from #{@pod.location.common_name}!"
+      redirect back
+    end
+
+    # create form for pods
+    get '/admin/pod/create' do
+      erb :pod_create
+    end
+
+    # edit form for pods
+    get '/admin/pods/:id/edit' do
+      pod_from_params
+      erb :pod_edit
+    end
+
+    # delete form for pods
+    get '/admin/pods/:id/delete' do
+      pod_from_params
+      erb :pod_delete
+    end
 
     get '/admin/users' do
       @modify_users = Optopus::User.all
