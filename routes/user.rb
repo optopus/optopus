d53007b6123af4f1b5a0ce684575c5ca1fe5994e
Allan Feid <allanfeid@gmail.com>
2013-01-29 16:42:03 -0500

    allow a user to change their display name

--- routes/user.rb
+++ routes/user.rb
@@ -2,7 +2,29 @@ module Optopus
   class App
     get '/user/:username' do
       @show_user = Optopus::User.where(:username => params[:username]).first
+      @admin_self = @user ? @show_user.id == @user.id : false
       erb :user
+    end
+
+    post '/user/:username/edit' do
+      @edit_user = Optopus::User.where(:username => params[:username]).first
+      unless @user && @edit_user.id == @user.id
+        flash[:error] = 'You are not authorized to edit other users.'
+        redirect back
+      end
+
+      if display_name = params.delete('display-name')
+        @edit_user.display_name = display_name
+      end
+
+      begin
+        @edit_user.save!
+        flash[:success] = 'Successfully updated your display name!'
+      rescue Exception => e
+        handle_error(e)
+      end
+
+      redirect back
     end
   end
 end
