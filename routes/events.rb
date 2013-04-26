632486fc35cf67a4ff33d517129d3ccf816e4284
Allan Feid <allanfeid@gmail.com>
2013-04-26 12:44:27 -0400

    expose a way to change how many events per page are listed

--- routes/events.rb
+++ routes/events.rb
@@ -1,7 +1,7 @@
 module Optopus
   class App
     get '/events' do
-      @events = Optopus::Event.order('created_at DESC').page(params[:page])
+      @events = Optopus::Event.order('created_at DESC').paginate(:page => params[:page], :per_page => (params[:per_page] || 50))
       erb :events
     end
   end
