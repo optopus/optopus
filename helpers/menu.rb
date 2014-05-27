376178ed4d9a42f01503ffe3e3350e44d1015e0c
Ben Nied <bnied@shutterstock.com>
2014-05-27 10:21:05 -0400

    Fixed menu helper to not crash.

--- helpers/menu.rb
+++ helpers/menu.rb
@@ -3,23 +3,31 @@ module Optopus
     helpers do
       # return menu sections relevant to current user only
       def menu_sections
-        Optopus::Menu.instance.sections.select do |section|
-          if section.required_role.nil? || section.required_role.lines.any? { |r| is_authorized?(r) }
-            section
-          end
-        end
+        get_authorized_sections(Optopus::Menu.instance.sections)
       end
 
       def utility_menu_sections
-        Optopus::UtilityMenu.instance.sections.select do |section|
-          if section.required_role.nil? || section.required_role.lines.any? { |r| is_authorized?(r) }
-            section
-          end
-        end
+        get_authorized_sections(Optopus::UtilityMenu.instance.sections)
       end
 
       def profile_menu_sections
         Optopus::ProfileMenu.instance.sections
+      end
+
+      def get_authorized_sections(sections)
+        sections.select do |section|
+          if not section.required_role.nil?
+            if section.required_role.kind_of?(String)
+              if section.required_role.lines.any? { |r| is_authorized?(r) }
+                section
+              end
+            else
+              if section.required_role.any? { |r| is_authorized?(r) }
+                section
+              end
+            end
+          end
+        end
       end
     end
   end
