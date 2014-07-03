f888bf3b3a0684b67851a80adef37a5c5d2a465b
Allan Feid <afeid@shutterstock.com>
2014-07-03 12:06:28 -0400

    Make a much better homepage.
    Split the more intensive graphs out into a reports page.

--- routes/main.rb
+++ routes/main.rb
@@ -6,11 +6,15 @@ module Optopus
 
     get '/' do
       @devices_to_be_provisioned = Optopus::Device.where(:provisioned => false)
-      @inactive_nodes = Optopus::Node.inactive.limit(5)
-      @new_nodes = Optopus::Node.where('created_at > ?', 7.days.ago).order('created_at DESC').limit(5)
-      @recent_events = Optopus::Event.order('created_at DESC').limit(5)
+      @inactive_nodes = Optopus::Node.inactive.limit(10)
+      @new_nodes = Optopus::Node.where('created_at > ?', 10.days.ago).order('created_at DESC').limit(10)
+      @recent_events = Optopus::Event.order('created_at DESC').limit(10)
       @active_node_count = Optopus::Node.active.count
       erb :index
+    end
+
+    get '/reports' do
+      erb :reports
     end
   end
 end
